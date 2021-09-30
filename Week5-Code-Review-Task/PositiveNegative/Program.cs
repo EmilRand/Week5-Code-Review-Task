1b6dfd7d58416f48113a88981f5e3c9f7ee5de33
Opilane TTHK <opilane@tthk.ee>
2021-09-30 08:37:31 +0300

    week-5 task

---
+++ Week5-Code-Review-Task/PositiveNegative/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PositiveNegative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*The program checks if the number is positive or negative*/
+            Console.WriteLine("Enter your number: ");
+
+            int userNumber = Int32.Parse(Console.ReadLine());          
+            if(userNumber > 0)
+            
+            {
+                Console.WriteLine("The number is positeive.");
+            } 
+             else if (userNumber < 0)
+             {
+                 Console.WriteLine("The number is negative.");
+             }
+             else
+             {
+                Console.WriteLine("Your number is 0");
+             }
+        }
+    }
+}
